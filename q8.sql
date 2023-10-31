856451cb732b516450b9ebe396bc2cf202de6b60
Gabriel Vainer <67750020+vai9er@users.noreply.github.com>
2023-10-31 03:28:29 -0400

    update q8 (not yet tested)

--- q8.sql
+++ q8.sql
@@ -1,74 +1,86 @@
--- Set the search path to the markus schema
-SET search_path TO markus;
+-- Never solo by choice.
 
--- Drop the table q8 if it already exists
+-- You must not change the next 2 lines or the table definition.
+SET search_path TO markus;
 DROP TABLE IF EXISTS q8 CASCADE;
 
--- Create the q8 table
 CREATE TABLE q8 (
 	username varchar(25) NOT NULL,
 	group_average real NOT NULL,
 	solo_average real DEFAULT NULL
 );
 
--- Drop views if they already exist
-DROP VIEW IF EXISTS AssignmentsWithGroups CASCADE;
-DROP VIEW IF EXISTS StudentSubmissionCounts CASCADE;
-DROP VIEW IF EXISTS MultiMemberGroups CASCADE;
-DROP VIEW IF EXISTS AvgGrades CASCADE;
+-- Identify Assignments with Solo and Group Options
+DROP VIEW IF EXISTS assignment_type CASCADE;
+CREATE VIEW assignment_type AS 
+SELECT 
+    assignment_id,
+    CASE 
+        WHEN group_max = 1 THEN 'solo'
+        ELSE 'group'
+    END as assignment_type
+FROM Assignment;
 
--- Create a view that lists assignments that allow groups
-CREATE VIEW AssignmentsWithGroups AS
-SELECT assignment_id
-FROM Assignment
-WHERE group_max > 1;
+-- Identify Students in Multi-Member Groups
+DROP VIEW IF EXISTS students_in_groups CASCADE;
+CREATE VIEW students_in_groups AS 
+SELECT 
+    m.username, 
+    ag.assignment_id
+FROM 
+    Membership m
+JOIN 
+    AssignmentGroup ag ON m.group_id = ag.group_id
+JOIN 
+    assignment_type at ON ag.assignment_id = at.assignment_id
+WHERE 
+    at.assignment_type = 'group'
+GROUP BY 
+    m.username, ag.assignment_id
+HAVING 
+    COUNT(m.username) > 1;
 
--- Create a view that counts submissions per student per assignment
-CREATE VIEW StudentSubmissionCounts AS
-SELECT S.username, AG.assignment_id, COUNT(S.submission_id) AS submission_count
-FROM Submissions S
-JOIN AssignmentGroup AG ON S.group_id = AG.group_id
-GROUP BY S.username, AG.assignment_id;
+-- Identify Students with Submissions for Every Assignment
+DROP VIEW IF EXISTS students_with_all_submissions CASCADE;
+CREATE VIEW students_with_all_submissions AS
+SELECT
+    s.username
+FROM
+    Submissions s
+JOIN
+    Assignment a ON s.assignment_id = a.assignment_id
+GROUP BY
+    s.username
+HAVING
+    COUNT(DISTINCT a.assignment_id) = (SELECT COUNT(*) FROM Assignment);
 
--- Create a view that identifies students who are in multi-member groups for assignments that allow it
-CREATE VIEW MultiMemberGroups AS
-SELECT M.username, AG.assignment_id
-FROM Membership M
-JOIN AssignmentGroup AG ON M.group_id = AG.group_id
-JOIN AssignmentsWithGroups A ON AG.assignment_id = A.assignment_id
-GROUP BY M.username, AG.assignment_id
-HAVING COUNT(M.username) > 1;
+-- Calculate Average Grades
+DROP VIEW IF EXISTS average_grades CASCADE;
+CREATE VIEW average_grades AS 
+SELECT 
+    swas.username,
+    AVG(CASE WHEN at.assignment_type = 'group' THEN r.mark ELSE NULL END) as group_average,
+    AVG(CASE WHEN at.assignment_type = 'solo' THEN r.mark ELSE NULL END) as solo_average
+FROM 
+    students_with_all_submissions swas
+JOIN 
+    students_in_groups sig ON swas.username = sig.username
+JOIN 
+    Membership m ON sig.username = m.username
+JOIN 
+    AssignmentGroup ag ON m.group_id = ag.group_id
+JOIN 
+    Result r ON ag.group_id = r.group_id
+JOIN 
+    assignment_type at ON ag.assignment_id = at.assignment_id
+GROUP BY 
+    swas.username;
 
--- Create a view that calculates average grades
-CREATE VIEW AvgGrades AS
-SELECT R.group_id, (R.mark / RI.weight_sum) * 100 AS average_grade
-FROM Result R
-JOIN (
-	SELECT assignment_id, SUM(weight) AS weight_sum
-	FROM RubricItem
-	GROUP BY assignment_id
-) RI ON R.group_id = RI.assignment_id;
-
--- Insert the required data into the q8 table
-WITH GroupAssignments AS (
-	SELECT username
-	FROM StudentSubmissionCounts S
-	JOIN AssignmentsWithGroups A ON S.assignment_id = A.assignment_id
-	GROUP BY S.username
-	HAVING COUNT(S.username) = (SELECT COUNT(*) FROM AssignmentsWithGroups)
-),
-SoloAssignments AS (
-	SELECT username
-	FROM StudentSubmissionCounts
-	WHERE assignment_id NOT IN (SELECT assignment_id FROM AssignmentsWithGroups)
-	GROUP BY username
-	HAVING COUNT(username) = (SELECT COUNT(*) FROM Assignment WHERE group_max = 1)
-)
-SELECT G.username,
-	   AVG(GA.average_grade) AS group_average,
-	   AVG(SA.average_grade) AS solo_average
-FROM GroupAssignments G
-LEFT JOIN MultiMemberGroups MMG ON G.username = MMG.username
-LEFT JOIN AvgGrades GA ON MMG.group_id = GA.group_id
-LEFT JOIN SoloAssignments SA ON G.username = SA.username
-GROUP BY G.username;
+-- Your query that answers the question goes below the "insert into" line:
+INSERT INTO q8
+SELECT 
+    a.username,
+    a.group_average,
+    a.solo_average
+FROM 
+    average_grades a;
