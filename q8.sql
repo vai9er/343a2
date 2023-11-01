1841135bf18a873c042286d9389a631ae9ec654c
Gabriel Vainer <67750020+vai9er@users.noreply.github.com>
2023-11-01 00:38:18 -0400

    update q8.sql

--- q8.sql
+++ q8.sql
@@ -1,86 +1,105 @@
--- Never solo by choice.
+    -- Never solo by choice.
 
--- You must not change the next 2 lines or the table definition.
-SET search_path TO markus;
-DROP TABLE IF EXISTS q8 CASCADE;
+    -- You must not change the next 2 lines or the table definition.
+    SET search_path TO markus;
+    DROP TABLE IF EXISTS q8 CASCADE;
 
-CREATE TABLE q8 (
-	username varchar(25) NOT NULL,
-	group_average real NOT NULL,
-	solo_average real DEFAULT NULL
-);
+    CREATE TABLE q8 (
+        username varchar(25) NOT NULL,
+        group_average real NOT NULL,
+        solo_average real DEFAULT NULL
+    );
 
--- Identify Assignments with Solo and Group Options
-DROP VIEW IF EXISTS assignment_type CASCADE;
-CREATE VIEW assignment_type AS 
-SELECT 
-    assignment_id,
-    CASE 
-        WHEN group_max = 1 THEN 'solo'
-        ELSE 'group'
-    END as assignment_type
-FROM Assignment;
+    -- Identify students who have submitted at least one file for every assignment
+    DROP VIEW IF EXISTS students_with_submissions CASCADE;
+    CREATE VIEW students_with_submissions AS
+    SELECT
+        m.username
+    FROM
+        Membership m
+    JOIN
+        Submissions s ON m.username = s.username AND m.group_id = s.group_id
+    JOIN
+        AssignmentGroup ag ON m.group_id = ag.group_id
+    GROUP BY
+        m.username
+    HAVING
+        COUNT(DISTINCT ag.assignment_id) = (SELECT COUNT(*) FROM Assignment);
 
--- Identify Students in Multi-Member Groups
-DROP VIEW IF EXISTS students_in_groups CASCADE;
-CREATE VIEW students_in_groups AS 
-SELECT 
-    m.username, 
-    ag.assignment_id
-FROM 
-    Membership m
-JOIN 
-    AssignmentGroup ag ON m.group_id = ag.group_id
-JOIN 
-    assignment_type at ON ag.assignment_id = at.assignment_id
-WHERE 
-    at.assignment_type = 'group'
-GROUP BY 
-    m.username, ag.assignment_id
-HAVING 
-    COUNT(m.username) > 1;
+    -- Identify groups that are multi-member
+    DROP VIEW IF EXISTS multi_member_groups CASCADE;
+    CREATE VIEW multi_member_groups AS
+    SELECT
+        ag.group_id,
+        ag.assignment_id
+    FROM
+        AssignmentGroup ag
+    JOIN
+        Membership m ON ag.group_id = m.group_id
+    GROUP BY
+        ag.group_id, ag.assignment_id
+    HAVING
+        COUNT(m.username) > 1;
 
--- Identify Students with Submissions for Every Assignment
-DROP VIEW IF EXISTS students_with_all_submissions CASCADE;
-CREATE VIEW students_with_all_submissions AS
-SELECT
-    s.username
-FROM
-    Submissions s
-JOIN
-    Assignment a ON s.assignment_id = a.assignment_id
-GROUP BY
-    s.username
-HAVING
-    COUNT(DISTINCT a.assignment_id) = (SELECT COUNT(*) FROM Assignment);
+    -- Identify students who have always been in multi-member groups for group assignments
+    DROP VIEW IF EXISTS students_in_multi_member_groups CASCADE;
+    CREATE VIEW students_in_multi_member_groups AS
+    SELECT
+        m.username
+    FROM
+        Membership m
+    JOIN
+        multi_member_groups mmg ON m.group_id = mmg.group_id
+    GROUP BY
+        m.username
+    HAVING
+        COUNT(DISTINCT mmg.assignment_id) = (SELECT COUNT(DISTINCT assignment_id) FROM multi_member_groups);
 
--- Calculate Average Grades
-DROP VIEW IF EXISTS average_grades CASCADE;
-CREATE VIEW average_grades AS 
-SELECT 
-    swas.username,
-    AVG(CASE WHEN at.assignment_type = 'group' THEN r.mark ELSE NULL END) as group_average,
-    AVG(CASE WHEN at.assignment_type = 'solo' THEN r.mark ELSE NULL END) as solo_average
-FROM 
-    students_with_all_submissions swas
-JOIN 
-    students_in_groups sig ON swas.username = sig.username
-JOIN 
-    Membership m ON sig.username = m.username
-JOIN 
-    AssignmentGroup ag ON m.group_id = ag.group_id
-JOIN 
-    Result r ON ag.group_id = r.group_id
-JOIN 
-    assignment_type at ON ag.assignment_id = at.assignment_id
-GROUP BY 
-    swas.username;
+    -- Calculate average grades for group assignments
+    DROP VIEW IF EXISTS group_assignment_avg_grades CASCADE;
+    CREATE VIEW group_assignment_avg_grades AS
+    SELECT
+        m.username,
+        AVG(r.mark) as group_avg
+    FROM
+        Membership m
+    JOIN
+        AssignmentGroup ag ON m.group_id = ag.group_id
+    JOIN
+        Result r ON ag.group_id = r.group_id
+    JOIN
+        multi_member_groups mmg ON ag.group_id = mmg.group_id
+    GROUP BY
+        m.username;
 
--- Your query that answers the question goes below the "insert into" line:
-INSERT INTO q8
-SELECT 
-    a.username,
-    a.group_average,
-    a.solo_average
-FROM 
-    average_grades a;
+    -- Calculate average grades for solo assignments
+    DROP VIEW IF EXISTS solo_assignment_avg_grades CASCADE;
+    CREATE VIEW solo_assignment_avg_grades AS
+    SELECT
+        m.username,
+        AVG(r.mark) as solo_avg
+    FROM
+        Membership m
+    JOIN
+        AssignmentGroup ag ON m.group_id = ag.group_id
+    JOIN
+        Result r ON ag.group_id = r.group_id
+    WHERE
+        NOT EXISTS (SELECT 1 FROM multi_member_groups mmg WHERE ag.group_id = mmg.group_id)
+    GROUP BY
+        m.username;
+
+    -- Insert the final results into q8
+    INSERT INTO q8 (username, group_average, solo_average)
+    SELECT
+        s.username,
+        COALESCE(g.group_avg, 0) as group_average,
+        so.solo_avg as solo_average
+    FROM
+        students_with_submissions s
+    LEFT JOIN
+        group_assignment_avg_grades g ON s.username = g.username
+    LEFT JOIN
+        solo_assignment_avg_grades so ON s.username = so.username
+    WHERE
+        s.username IN (SELECT username FROM students_in_multi_member_groups);
