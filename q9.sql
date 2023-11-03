43393c26528ee0fdc8eecf11ff03cad6c5ebe235
Kevin Xiong <kevin.xiong51800@gmail.com>
2023-11-03 04:23:22 -0400

    bug fix for q9.sql

--- q9.sql
+++ q9.sql
@@ -8,7 +8,7 @@
 -- Consequently, you can use comparison operators (>, <, =, ≥, ≤) 
 -- or ORDER BY on string attributes in the same way you use them on integer 
 -- attributes. For example, the result of select ’apple’ > ’banana’; would be False, 
---since ’apple’ would appear first in the dictionary.
+--since ’apple’ would appear first in the dictionary. 
 
 -- You must not change the next 2 lines or the table definition.
 SET search_path TO markus;
@@ -23,6 +23,7 @@ CREATE TABLE q9 (
 -- (But give them better names!) The IF EXISTS avoids generating an error
 -- the first time this file is imported.
 DROP VIEW IF EXISTS multi_assignments CASCADE;
+DROP VIEW IF EXISTS loners_groups CASCADE;
 DROP VIEW IF EXISTS loners CASCADE;
 DROP VIEW IF EXISTS never_alone CASCADE;
 DROP VIEW IF EXISTS impossible_pairs CASCADE;
@@ -36,14 +37,19 @@ Select assignment_id from Assignment
 WHERE group_max > 1;
 
 -- Students who worked alone in multi_assignments
-Create View loners AS
-Select Membership.username
+Create View loners_groups AS
+Select Membership.group_id
 From AssignmentGroup JOIN multi_assignments
 	ON multi_assignments.assignment_id = AssignmentGroup.assignment_id
 	JOIN Membership
 	ON Membership.group_id = AssignmentGroup.group_ID
-Group by Membership.group_id, Membership.username
-Having count(Membership.username) = 1;
+Group by Membership.group_id
+Having count(Membership.group_id) = 1;
+
+Create View loners AS
+Select Membership.username
+From loners_groups JOIN Membership
+	on Membership.group_id = loners_groups.group_id;
 
 -- Students who nevver worked alone on multi_assignments
 Create View never_alone AS
@@ -54,24 +60,27 @@ From AssignmentGroup JOIN multi_assignments
 	ON Membership.group_id = AssignmentGroup.group_ID
 Where Membership.username NOT IN (SELECT username FROM loners);
 
-
 -- All impossible pairs
 Create View impossible_pairs AS
 Select distinct N1.username as student1, N2.username as student2
 From never_alone N1, never_alone N2
-Where N1.assignment_id = N2.assignment_id and N1.username != N2.username and N1.group_id != N2.group_id;
+Where (N1.assignment_id = N2.assignment_id and N1.username != N2.username and N1.group_id != N2.group_id);
 
 -- All possible pairs
 Create View possible_pairs AS
 Select Distinct N1.username as student1, N2.username as student2
 From never_alone N1, never_alone N2
-Where N1.username != N2.username;
+Where N1.username != N2.username
+  AND N1.username not in (select username from loners)
+  AND N2.username not in (select username from loners);
 
 -- Inseperable
 Create View Inseperable AS
 Select student1, student2
 from possible_pairs
-where (student1, student2) NOT IN (SELECT student1, student2 FROM impossible_pairs);
+where (student1, student2) NOT IN (SELECT student1, student2 FROM impossible_pairs) 
+and student1 < student2
+order by student1 asc;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q9 (student1, student2)
